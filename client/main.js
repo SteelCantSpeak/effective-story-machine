6fb3ae1152f458ee9ec2898b83888d98aae272dc
SteelCantSpeak <103188993+SteelCantSpeak@users.noreply.github.com>
2024-08-04 06:00:59 +0000

    Create Server/Runtime env. TODO: Mechanics

---
+++ client/main.js
@@ -0,0 +1,80 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import Stats from 'three/addons/libs/stats.module.js';
+import * as Objects from './runtime.js'
+
+
+let scene, camera, renderer, controls, clock, stats;
+
+//Create Scene. 
+function init(){
+    
+    //Create Visuals
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.z = 10;
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    Objects.initialiseObjects();
+
+    clock = new THREE.Clock();
+    
+    controls = new OrbitControls( camera, renderer.domElement );
+				controls.listenToKeyEvents( window ); // optional
+
+				//controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+				controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+				controls.dampingFactor = 0.05;
+
+				controls.screenSpacePanning = false;
+
+				controls.minDistance = 10;
+				controls.maxDistance = 20;
+
+				controls.maxPolarAngle = Math.PI / 2;
+    
+                // Create cube
+    
+
+stats = new Stats();
+				document.body.appendChild( stats.dom );
+
+
+                scene.add( Objects.group );
+                scene.add( Objects.lights );
+renderer.setAnimationLoop( animate );
+}
+
+// Render loop
+function animate() {
+
+    const delta = clock.getDelta();
+
+	//group.rotation.y += 0.25 * delta;
+    Objects.updateObjects();
+
+    controls.update( clock.getDelta() );
+    stats.update();
+    renderer.render(scene, camera);
+}
+
+
+// Resize handling
+window.addEventListener('resize', function() {
+    const aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = aspect;
+    camera.updateProjectionMatrix();
+    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+try {
+    init();
+  }
+  catch(err) {
+    console.error("Try again Later, Nerd"+ err);
+  }
